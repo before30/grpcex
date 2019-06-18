b029d9a9ded487520e8b62ebedccc1e145c86c5b
Joseph <before30@gmail.com>
2019-06-18 23:11:55 +0900

    Add baseline

---
+++ src/main/java/cc/before30/home/grpcex/GrpcexApplication.java
@@ -0,0 +1,13 @@
+package cc.before30.home.grpcex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cc/before30/home/grpcex/GrpcexApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.before30.home.grpcex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
